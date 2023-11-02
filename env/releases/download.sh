baa10421b495c9031a49feaa575e010d8ceef567
Tarsis <br.tmvdl@gmail.com>
2023-11-02 10:53:39 -0300

    Updating relese script files

--- env/releases/download.sh
+++ env/releases/download.sh
@@ -1,3 +1,9 @@
-rm -rf ./.github/*.png
+#!/usr/bin/sh
 
-gh release download -D ./.github/ $@
+path="${2:-downloads}"
+
+rm -rf "${path}"
+
+mkdir -p "${path}"
+
+gh release download -D "${path}" "${1}"
